9edd194bac4f91ded204832742e658e0c0d5ca46
Robert Ochoa <ochoa.robert1@gmail.com>
2024-07-28 18:10:15 -0400

    Portfolio created

--- templates/home/portfolio.php
+++ templates/home/portfolio.php
@@ -3,47 +3,32 @@
         <?php $portfolio_title = ro_meta_value('ro_home_portfolio_title'); ?>
         <h2><?php echo esc_html($portfolio_title); ?></h2>
     </header>
-    <?php $arr_portfolio = new WP_Query([
-        'post_type' => 'casos',
-        'posts_per_page' => -1,
-        'orderby' => 'title',
-        'order' => 'ASC',
-        'tax_query' => [[
-            'taxonomy' => 'categoria-casos',
-            'field' => 'slug',
-            'terms' => ['destacado']
-        ]]
-    ]); ?>
-    <?php if ($arr_portfolio->have_posts()) : ?>
+    <?php $i = 0; ?>
+    <?php $arr_boxes = ro_meta_value('ro_home_portfolio_boxes'); ?>
+    <?php $arr_portfolio = ro_meta_value('ro_home_portfolio_selected'); ?>
+    <?php if (is_array($arr_portfolio)) : ?>
     <div class="main-success-portfolio-container">
-        <?php while ($arr_portfolio->have_posts()) :
-            $arr_portfolio->the_post(); ?>
-        <article id="<?php the_ID() ?>" class="main-success-portfolio-item">
+        <?php foreach ($arr_portfolio as $post) : ?>
+            <?php setup_postdata($post); ?>
+        <article id="<?php echo esc_attr(get_the_ID()); ?>" class="main-success-portfolio-item">
             <picture>
-                <?php the_post_thumbnail('full', ['class' => 'response-class']); ?>
+                <a href="<?php echo esc_url(get_permalink());?>" title="<?php esc_attr_e('Ver más acerca de este caso de éxito', 'robertochoa'); ?>">
+                  <?php the_post_thumbnail('full', ['class' => 'response-class']);
+                    ?>
+                </a>
             </picture>
-            <header>
-                <h3><?php the_title(); ?></h3>
-                <div class="category-list">
-                    <?php $terms = get_the_terms(get_the_ID(), 'categoria-casos'); ?>
-                    <?php if ($terms) : ?>
-                        <?php foreach ($terms as $item) : ?>
-                            <?php if ($item->name !== 'Destacado') : ?>
-                                <?php $item_link = (get_term_link($item) !== '') ? get_term_link($item) : ''; ?>
-                                <?php if ($item_link !== '') : ?>
-                                    <a href="<?php echo esc_url($item_link); ?>" class="category-item"><?php echo esc_html($item->name); ?></a>
-                                <?php endif; ?>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                </div>
-                <a href="<?php the_permalink(); ?>" class="btn-portfolio"><?php esc_html_e('Ver Caso de Éxito', 'robertochoa'); ?></a>
-            </header>
         </article>
-        <?php endwhile; ?>
+        <article class="main-success-portfolio-item main-success-portfolio-item-text">
+            <div class="portfolio-item-text-wrapper">
+                <h3><?php echo esc_html($arr_boxes[$i]['title']); ?></h3>
+                <div class="percentage"><?php echo esc_html($arr_boxes[$i]['number']); ?></div>
+            </div>
+        </article>
+            <?php $i++;
+        endforeach; ?>
     </div>
     <?php endif; ?>
-    <?php wp_reset_query(); ?>
+    <?php wp_reset_postdata(); ?>
     <div class="main-success-content-button" itemprop="potentialAction" itemscope itemtype="http://schema.org/CommunicateAction">
         <?php $btn_link = ro_meta_value('ro_home_portfolio_btn_link'); ?>
         <?php $btn_text = ro_meta_value('ro_home_portfolio_btn_text'); ?>
