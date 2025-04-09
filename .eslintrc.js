237fd66b98be31f8e44a924b3604f2c018c4f329
Robert Ochoa <ochoa.robert1@gmail.com>
2025-04-09 13:53:39 -0400

    Add ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,11 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
+  root: !0,
+  env: { browser: !0, es2021: !0, node: !0 },
+  extends: ["airbnb-base"],
+  parserOptions: { ecmaVersion: 12, sourceType: "module" },
   rules: {
-    'no-console': 'warn',
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'linebreak-style': ['error', 'windows'],
+    "no-console": "warn",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: !0 }],
+    "linebreak-style": ["error", "unix"],
   },
 };
